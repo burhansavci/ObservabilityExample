d6247ee8fc96c344fa836af9d7136db8a09af5d4
Burhan SAVCI <51991915+burhansavci@users.noreply.github.com>
2021-02-11 18:08:18 +0300

    Add Jaeger with using OpenTelemetry

---
+++ ObservabilityExample.Infrastructure/Jaeger/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ObservabilityExample.Services.Products;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ObservabilityExample.Infrastructure.Jaeger
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddJaeger(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jaegerOptions = new JaegerOptions();
+            configuration.GetSection("Jaeger").Bind(jaegerOptions);
+
+            services.AddOpenTelemetryTracing(config => config
+                                                      .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                                                                                         .AddService(jaegerOptions.ServiceName))
+                                                      .SetSampler(GetSampler(jaegerOptions))
+                                                      .AddJaegerExporter(c =>
+                                                       {
+                                                           c.AgentHost = jaegerOptions.Host;
+                                                           c.AgentPort = jaegerOptions.Port;
+                                                           c.MaxPayloadSizeInBytes = jaegerOptions.MaxPayloadSizeInBytes;
+                                                       })
+                                                      .AddSqlClientInstrumentation(opt => opt.SetDbStatementForText = true)
+                                                      .AddAspNetCoreInstrumentation(x =>
+                                                               x.Filter = context =>
+                                                                       !jaegerOptions.ExcludePaths.Contains(context.Request.Path.Value,
+                                                                               StringComparer.OrdinalIgnoreCase))
+                                                      .AddHttpClientInstrumentation());
+
+            return services;
+        }
+
+        private static Sampler GetSampler(JaegerOptions jaegerOptions)
+        {
+            return jaegerOptions.Sampler switch
+                   {
+                           "alwaysOn" => new AlwaysOnSampler(),
+                           "alwaysOff" => new AlwaysOffSampler(),
+                           "probabilistic" => new TraceIdRatioBasedSampler(jaegerOptions.SamplingRate),
+                           _ => new AlwaysOnSampler()
+                   };
+        }
+    }
+}
---
+++ ObservabilityExample.Infrastructure/Jaeger/JaegerOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ObservabilityExample.Services.Products
+{
+    public class JaegerOptions
+    {
+        public string ServiceName { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public int MaxPayloadSizeInBytes { get; set; } = 4096;
+        public string Sampler { get; set; }
+        public double MaxTracesPerSecond { get; set; } = 5;
+        public double SamplingRate { get; set; } = 0.2;
+        public IEnumerable<string> ExcludePaths { get; set; }
+    }
+}
