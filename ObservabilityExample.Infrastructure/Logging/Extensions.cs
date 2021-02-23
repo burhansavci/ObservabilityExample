2ab80d015db779626b8236e7d6f75305c6fd6220
Burhan SAVCI <51991915+burhansavci@users.noreply.github.com>
2021-02-23 13:14:32 +0300

    Add file logging

--- ObservabilityExample.Infrastructure/Logging/Extensions.cs
+++ ObservabilityExample.Infrastructure/Logging/Extensions.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Filters;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
 
 namespace ObservabilityExample.Infrastructure.Logging
 {
@@ -15,10 +17,10 @@ namespace ObservabilityExample.Infrastructure.Logging
                 var appOptions = context.Configuration.GetOptions<AppOptions>("App");
                 var seqOptions = context.Configuration.GetOptions<SeqOptions>("Seq");
                 var serilogOptions = context.Configuration.GetOptions<SerilogOptions>("Serilog");
+                var fileOptions = context.Configuration.GetOptions<FileOptions>("File");
+
                 if (!Enum.TryParse<LogEventLevel>(serilogOptions.Level, true, out var level))
-                {
                     level = LogEventLevel.Information;
-                }
 
                 applicationName = string.IsNullOrWhiteSpace(applicationName) ? appOptions.Name : applicationName;
                 loggerConfiguration.Enrich.FromLogContext()
@@ -29,16 +31,28 @@ namespace ObservabilityExample.Infrastructure.Logging
                 serilogOptions.ExcludePaths?.ToList().ForEach(p => loggerConfiguration.Filter
                                                                                       .ByExcluding(Matching.WithProperty<string>("RequestPath",
                                                                                                n => n.EndsWith(p))));
-                Configure(loggerConfiguration, seqOptions, serilogOptions);
+                Configure(loggerConfiguration, seqOptions, serilogOptions,
+                        fileOptions);
             });
 
-        private static void Configure(LoggerConfiguration loggerConfiguration, SeqOptions seqOptions, SerilogOptions serilogOptions)
+        private static void Configure(LoggerConfiguration loggerConfiguration, SeqOptions seqOptions, SerilogOptions serilogOptions,
+                                      FileOptions fileOptions)
         {
             if (seqOptions.Enabled)
                 loggerConfiguration.WriteTo.Seq(seqOptions.Url, apiKey: seqOptions.ApiKey);
 
             if (serilogOptions.ConsoleEnabled)
                 loggerConfiguration.WriteTo.Console();
+
+            if (fileOptions.Enabled)
+            {
+                var path = string.IsNullOrWhiteSpace(fileOptions.Path) ? "logs/logs.log" : fileOptions.Path;
+
+                if (!Enum.TryParse<RollingInterval>(fileOptions.Interval, true, out var interval))
+                    interval = RollingInterval.Day;
+
+                loggerConfiguration.WriteTo.File(new JsonFormatter(), path, rollingInterval: interval);
+            }
         }
     }
 }
---
+++ ObservabilityExample.Infrastructure/Logging/FileOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ObservabilityExample.Infrastructure.Logging
+{
+    public class FileOptions
+    {
+        public bool Enabled { get; set; }
+        public string Path { get; set; }
+        public string Interval { get; set; }
+    }
+}
