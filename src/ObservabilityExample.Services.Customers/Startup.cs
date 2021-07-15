e507d8ce196398c7687a94846ae0be952f4c1fdc
Burhan SAVCI <burhansavci@gmail.com>
2021-07-15 23:50:50 +0300

    Add docker support

--- src/ObservabilityExample.Services.Customers/Startup.cs
+++ src/ObservabilityExample.Services.Customers/Startup.cs
@@ -48,7 +48,7 @@ namespace ObservabilityExample.Services.Customers
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("docker"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
--- src/ObservabilityExample.Services.Products/Startup.cs
+++ src/ObservabilityExample.Services.Products/Startup.cs
@@ -41,13 +41,12 @@ namespace ObservabilityExample.Services.Products
             services.AddRabbitMq(Configuration);
             services.AddJaeger(Configuration);
             services.AddPrometheus(Configuration);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("docker"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
